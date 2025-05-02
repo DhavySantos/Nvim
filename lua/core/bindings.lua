81865f69d1735b07c3732b6ad028f0fab08f4444
Dhavy Santos <dhavyzhn@gmail.com>
2025-05-02 15:15:37 -0300

    feat: add a keybind to clear search highlight

--- lua/core/bindings.lua
+++ lua/core/bindings.lua
@@ -8,6 +8,8 @@ vim.keymap.set("n", "<leader>a", "<C-w>h", opts);
 vim.keymap.set("n", "<leader>s", "<C-w>j", opts);
 vim.keymap.set("n", "<leader>d", "<C-w>l", opts);
 
+vim.keymap.set("n", "<esc>", ":nohlsearch<cr>", opts);
+
 -- Buffer navigation
 vim.keymap.set("n", "<leader>x", ":lua MiniBufremove.delete()<cr>", opts);
 vim.keymap.set("n", "<leader>q", ":bprevious<CR>", opts);
