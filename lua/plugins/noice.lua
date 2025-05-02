8bf0eaba87a45d9f319dc02587a97e4599d4622b
Dhavy Santos <dhavyzhn@gmail.com>
2025-05-02 15:16:53 -0300

    feat: set noice to render lsp progress using notify

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -9,6 +9,23 @@ return {
 	},
 
 	opts = {
+		messages = {
+			enabled = false
+		},
+
+		views = {
+			notify = {
+				replace = true
+			}
+		},
+
+		lsp = {
+			progress = {
+				view = "notify",
+				enable = true,
+			}
+		},
+
 		presets = {
 			command_palette = true,
 			lsp_doc_border = true,
