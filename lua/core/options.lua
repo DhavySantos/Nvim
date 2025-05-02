5ed97ad50a3eace71585131a02fc40a91719fa1c
Dhavy Santos <dhavyzhn@gmail.com>
2025-05-01 21:33:54 -0300

    feat: set custom neovim options

---
+++ lua/core/options.lua
@@ -0,0 +1,22 @@
+vim.cmd.colorscheme("habamax");
+
+-- DISPLAY CONFIGURATION --
+vim.opt.termguicolors = true;
+vim.opt.cursorline = true;
+vim.opt.splitright = true;
+vim.opt.splitbelow = true;
+vim.opt.scrolloff = 99;
+vim.opt.pumheight = 10;
+vim.opt.cmdheight = 0;
+vim.opt.number = true;
+vim.opt.wrap = false;
+
+-- FILES CONFIGURATION --
+vim.opt.swapfile = false;
+vim.opt.undofile = true;
+vim.opt.backup = false;
+
+-- INDENT CONFIGURATION --
+vim.opt.smartindent = true;
+vim.opt.shiftwidth = 2;
+vim.opt.tabstop = 2;
